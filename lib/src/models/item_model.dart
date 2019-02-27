16c045a4350e195d35461643b0f037257ccd2fbb
Louis Chang <dog8314@gmail.com>
2019-02-27 12:21:39 +0800

    Add item model constructor

--- lib/src/models/item_model.dart
+++ lib/src/models/item_model.dart
@@ -1,6 +1,4 @@
-
 class ItemModel {
-
   final int id;
   final bool deleted;
   final String type;
@@ -14,4 +12,19 @@ class ItemModel {
   final int score;
   final String title;
   final int descendants;
+
+  ItemModel.fromJson(Map<String, dynamic> parserJson)
+      : id = parserJson['id'],
+        deleted = parserJson['deleted'],
+        type = parserJson['type'],
+        by = parserJson['by'],
+        time = parserJson['time'],
+        text = parserJson['text'],
+        dead = parserJson['dead'],
+        parent = parserJson['parent'],
+        kids = parserJson['kids'],
+        url = parserJson['url'],
+        score = parserJson['score'],
+        title = parserJson['title'],
+        descendants = parserJson['descendants'];
 }
