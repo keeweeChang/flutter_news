31b657a2d300c7e53701aff52017f129e164b9b8
Louis Chang <dog8314@gmail.com>
2019-03-11 14:40:07 +0800

    Create newsItems sqlite Db

--- lib/src/models/item_model.dart
+++ lib/src/models/item_model.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 class ItemModel {
   final int id;
   final bool deleted;
@@ -27,4 +29,37 @@ class ItemModel {
         score = parserJson['score'],
         title = parserJson['title'],
         descendants = parserJson['descendants'];
+
+  ItemModel.fromDb(Map<String, dynamic> parserJson)
+      : id = parserJson['id'],
+        deleted = parserJson['deleted'] == 1,
+        type = parserJson['type'],
+        by = parserJson['by'],
+        time = parserJson['time'],
+        text = parserJson['text'],
+        dead = parserJson['dead'] == 1,
+        parent = parserJson['parent'],
+        kids = jsonDecode(parserJson['kids']),
+        url = parserJson['url'],
+        score = parserJson['score'],
+        title = parserJson['title'],
+        descendants = parserJson['descendants'];
+
+  Map<String, dynamic> toMapForDb() {
+    return <String, dynamic>{
+      "id": id,
+      "deleted": deleted ? 1 : 0,
+      "type": type,
+      "by": by,
+      "time": time,
+      "text": text,
+      "dead": dead ? 1 : 0,
+      "parent": parent,
+      "kids": jsonEncode(kids),
+      "url": url,
+      "score": score,
+      "title": title,
+      "descendants": descendants,
+    };
+  }
 }
