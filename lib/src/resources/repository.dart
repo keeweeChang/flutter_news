566069bd94655c319feff19ca1183bd37b27ad13
Louis Chang <dog8314@gmail.com>
2019-03-11 14:57:01 +0800

    Create repository

---
+++ lib/src/resources/repository.dart
@@ -0,0 +1,22 @@
+import 'news_api_provider.dart';
+import 'news_db_provider.dart';
+import '../models/item_model.dart';
+
+class Repository {
+  NewsApiProvider apiProvider = NewsApiProvider();
+  NewsDbProvider dbProvider = NewsDbProvider();
+  fetchTopIds() {
+    return apiProvider.fetchTopIds();
+  }
+
+  fetchItem(int id) async {
+    var item = await dbProvider.fetchItem(id);
+    if (item != null) {
+      return item;
+    }
+
+    item = apiProvider.fetchItem(id);
+    dbProvider.addItem(item);
+    return item;
+  }
+}
