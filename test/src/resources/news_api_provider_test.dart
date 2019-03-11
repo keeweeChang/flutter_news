06db09f1a065c4834704d4dca86be46772d9f7ec
Louis Chang <dog8314@gmail.com>
2019-03-11 14:16:38 +0800

    add fetchItems test

--- test/src/resources/news_api_provider_test.dart
+++ test/src/resources/news_api_provider_test.dart
@@ -13,4 +13,13 @@ void main() {
     final ids = await newsApi.fetchTopIds();
     expect(ids, [1, 2, 3, 4]);
   });
+
+  test("FetchItem retruns a item info", () async {
+    final newsApi = NewsApiProvider();
+    newsApi.client = MockClient((requset) async {
+      return Response(json.encode({"id": 123}), 200);
+    });
+    final item = await newsApi.fetchItem(999);
+    expect(123, item.id);
+  });
 }
