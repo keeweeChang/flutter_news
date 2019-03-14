5cdf120a44348a8380747a356e2afb85f81b198b
Louis Chang <dog8314@gmail.com>
2019-03-14 14:14:49 +0800

    Add rxDart and show all TopIds

--- lib/src/blocs/stories_provider.dart
+++ lib/src/blocs/stories_provider.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 import 'stories_blocs.dart';
-
+export 'stories_blocs.dart';
 class StoriesProvider extends InheritedWidget {
   final StoriesBloc bloc;
   @override
   bool updateShouldNotify(_) => true;
 
-  StoriesProvider(Key key, Widget child)
+  StoriesProvider({Key key, Widget child})
       : bloc = StoriesBloc(),
         super(key: key, child: child);
 
--- lib/src/screens/news_list.dart
+++ lib/src/screens/news_list.dart
@@ -1,13 +1,35 @@
 import 'package:flutter/material.dart';
+import '../blocs/stories_provider.dart';
 
 class NewsList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final bloc = StoriesProvider.of(context);
+    //Not Good
+    bloc.fetchTopIds();
+    print("fetchTopIds");
     return Scaffold(
-        appBar: AppBar(
-          title: Text("News"),
-        ),
-        body: Text("show some news"),
-      );
+      appBar: AppBar(
+        title: Text("News"),
+      ),
+      body: _showList(bloc),
+    );
+  }
+
+  _showList(StoriesBloc bloc) {
+    return StreamBuilder(
+      stream: bloc.topIds,
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) {
+          return Text("Still waiting data");
+        } else {
+          return ListView.builder(
+              itemCount: snapshot.data.length,
+              itemBuilder: (context, index) {
+                return Text("${snapshot.data[index]}");
+              });
+        }
+      },
+    );
   }
 }
