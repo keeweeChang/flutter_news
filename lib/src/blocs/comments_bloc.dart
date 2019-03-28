f1b4576aba4c33956b8ec7b3c1c86a6b3a486226
Louis Chang <dog8314@gmail.com>
2019-03-28 17:26:22 +0800

    Add CommentsBloc memeber variable

--- lib/src/blocs/comments_bloc.dart
+++ lib/src/blocs/comments_bloc.dart
@@ -1,3 +1,21 @@
 import 'dart:async';
+import 'package:rxdart/rxdart.dart';
+import '../models/item_model.dart';
 
-class CommentsBloc {}
+class CommentsBloc {
+  final _commentsFetcher = PublishSubject<int>();
+  final _commentsOutput = BehaviorSubject<Map<int, Future<ItemModel>>>();
+
+  Observable<Map<int, Future<ItemModel>>> get itemWithComments =>
+      _commentsOutput.stream;
+  Function(int id) get fetchItemWithComments => _commentsFetcher.sink.add;
+
+  CommentsBloc() {
+    
+  }
+
+  close() {
+    _commentsFetcher.close();
+    _commentsOutput.close();
+  }
+}
