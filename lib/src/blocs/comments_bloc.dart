457d75984a7069f712f5970f6f649eb130a70cf6
Louis Chang <dog8314@gmail.com>
2019-03-29 11:58:51 +0800

    Add comments scanTransformer

--- lib/src/blocs/comments_bloc.dart
+++ lib/src/blocs/comments_bloc.dart
@@ -1,8 +1,10 @@
 import 'dart:async';
 import 'package:rxdart/rxdart.dart';
 import '../models/item_model.dart';
+import '../resources/repository.dart';
 
 class CommentsBloc {
+  final _repository = Repository();
   final _commentsFetcher = PublishSubject<int>();
   final _commentsOutput = BehaviorSubject<Map<int, Future<ItemModel>>>();
 
@@ -11,7 +13,20 @@ class CommentsBloc {
   Function(int id) get fetchItemWithComments => _commentsFetcher.sink.add;
 
   CommentsBloc() {
-    
+    _commentsFetcher.transform(_scanTransformer()).pipe(_commentsOutput);
+  }
+
+  _scanTransformer() {
+    return ScanStreamTransformer<int, Map<int, Future<ItemModel>>>(
+      (cache, id, count) {
+        cache[id] = _repository.fetchItem(id);
+        cache[id].then((item) {
+          item.kids.forEach((kidId) => fetchItemWithComments(kidId));
+        });
+        return cache;
+      },
+      <int, Future<ItemModel>>{},
+    );
   }
 
   close() {
